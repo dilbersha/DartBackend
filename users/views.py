eefaf1175f8be31c9d0e352151f8c1b65f60133f
abhinavprasad47 <abhinavprasad791@gmail.com>
2020-04-30 02:57:58 +0530

    regex type filtering with django_filter set

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django_filters import rest_framework as filters
+
 
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -6,11 +8,16 @@ from .models import *
 from .serializers import *
 
 # Create your views here.
+class ProductFilter(filters.FilterSet):
+
+    category = filters.CharFilter(lookup_expr='icontains')
+    name = filters.CharFilter(lookup_expr='icontains')
 class ProductViewset(viewsets.ModelViewSet):  
     
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filterset_fields = ('id','category')
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ProductFilter
 
     def perform_create(self, serializer):
         serializer.save()
