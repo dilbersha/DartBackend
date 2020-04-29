97b0b72b2f34bba369dff7dd5adf9faf60ef3fce
Jyothis P <jyothisp52@gmail.com>
2020-04-29 14:21:02 +0530

    Nullable fields for the product model.

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     unit = models.CharField(max_length=20)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, null=True)
     category = models.CharField(max_length=20)
     image_url = models.CharField(max_length=2048, null=True)
     in_stock = models.BooleanField(null=True)
