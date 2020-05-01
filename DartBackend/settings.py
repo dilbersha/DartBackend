02bde18b9ed4a6940a59c3c3893550e3f009f264
Kiran Johns <kiranjohns369@gmail.com>
2020-05-01 13:47:18 +0530

    user signup issue

--- DartBackend/settings.py
+++ DartBackend/settings.py
@@ -149,4 +149,13 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
-ACCOUNT_FORMS = {'signup': 'users.forms.MyCustomSignupForm'}
+ACCOUNT_FORMS = {
+    'login': 'allauth.account.forms.LoginForm',
+    'signup': 'users.forms.CustomSignupForm',
+    'add_email': 'allauth.account.forms.AddEmailForm',
+    'change_password': 'allauth.account.forms.ChangePasswordForm',
+    'set_password': 'allauth.account.forms.SetPasswordForm',
+    'reset_password': 'allauth.account.forms.ResetPasswordForm',
+    'reset_password_from_key': 'allauth.account.forms.ResetPasswordKeyForm',
+    'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
+}
--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,28 @@
 from allauth.account.forms import SignupForm
+from django import forms
 
-class MyCustomSignupForm(SignupForm):
+USER_CHOICES = [
+    ("customer","customer"),
+    ("shopkeeper","shopkeeper"),
+    ("rider","rider")
+]
 
-    def __init__(self, *args, **kwargs):
-        super(MyCustomSignupForm, self).__init__(*args, **kwargs)
-        self.fields['user_type'] = forms.CharField(required=True)
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+    phone = forms.CharField(max_length=14, label='Phone Number')
+    user_type = forms.ChoiceField(choices=USER_CHOICES)
 
-    def save(self, request):
-        user_type = self.cleaned_data.pop('user_type')
-        user = super(MyCustomSignupForm, self).save(request)
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.phone = self.cleaned_data['phone']
+        user.user_type = self.cleaned_data['user_type']
+        user.save()
+        return user
+
+    def save(self):
+
+        user = super(CustomSignupForm, self).save()
         return user
